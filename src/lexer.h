70247f17ded3a4e9f3bf3417e65b21b16bc1fef6
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-30 02:46:26 -0500

    Add `lexer.h`

---
+++ src/lexer.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * lexer.h -- rudimentary lexer
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_LEXER_H
+#define JKCC_LEXER_H
+
+
+#endif  /* JKCC_LEXER_H */
