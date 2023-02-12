33b09c68e27e45cd28980bd351b13fbf61a82441
Jacob Koziej <jacobkoziej@gmail.com>
2023-02-12 00:12:56 -0500

    Add `string.h`

---
+++ include/jkcc/string.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * string.h -- string type
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_STRING_H
+#define JKCC_STRING_H
+
+
+#endif  /* JKCC_STRING_H */
