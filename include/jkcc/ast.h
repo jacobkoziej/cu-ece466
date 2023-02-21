31fc0eede4cd0caaceba524a2e5ff640e5ba4ca4
Jacob Koziej <jacobkoziej@gmail.com>
2023-02-20 23:47:44 -0500

    Add `ast.h`

---
+++ include/jkcc/ast.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * ast.h -- abstract syntax tree
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_AST_H
+#define JKCC_AST_H
+
+
+#endif  /* JKCC_AST_H */
