96dd2beb308613fb1cc84f1a19adbb9b5f15030f
Jacob Koziej <jacobkoziej@gmail.com>
2023-02-22 01:23:27 -0500

    Add `jkcc.h`

---
+++ include/jkcc/jkcc.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * jkcc.h -- Jacob Koziej's C Compiler
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_JKCC_H
+#define JKCC_JKCC_H
+
+
+#endif  /* JKCC_JKCC_H */
