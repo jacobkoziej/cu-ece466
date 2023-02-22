ea9d4b1cfa673e721494ef2e45b53bab9dc619d6
Jacob Koziej <jacobkoziej@gmail.com>
2023-02-22 01:17:07 -0500

    Add `parser.h`

---
+++ include/jkcc/parser.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * parser.h -- rudimentary parser
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_PARSER_H
+#define JKCC_PARSER_H
+
+
+#endif  /* JKCC_PARSER_H */
