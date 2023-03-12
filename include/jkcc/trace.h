94f20a1e19551aa8e0cbeecc9715aa8291b387eb
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-12 16:11:35 -0400

    Add `trace.h`

---
+++ include/jkcc/trace.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * trace.h -- trace program execution
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_TRACE_H
+#define JKCC_TRACE_H
+
+
+#endif  /* JKCC_TRACE_H */
