d3cb44a49b2e667bccce35abd6b2f2ff574e083a
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-13 01:27:44 -0400

    Add option to enable execution traces

--- include/jkcc/jkcc.h
+++ include/jkcc/jkcc.h
@@ -10,15 +10,19 @@
 
 #include <stddef.h>
 
+#include <jkcc/trace.h>
+
 
 typedef struct jkcc_config_s {
 	unsigned ansi_sgr_stdout : 1;
 	unsigned ansi_sgr_stderr : 1;
+	unsigned trace           : 1;
 } jkcc_config_t;
 
 typedef struct jkcc_s {
 	const char    **file;
 	size_t          file_count;
+	trace_t         trace;
 	jkcc_config_t   config;
 } jkcc_t;
 
--- src/main.c
+++ src/main.c
@@ -13,11 +13,13 @@
 #include <unistd.h>
 
 #include <jkcc/jkcc.h>
+#include <jkcc/trace.h>
 
 #include "vcs-tag.h"
 
 
 #define KEY_COLOR 257
+#define KEY_TRACE 258
 
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state);
@@ -28,10 +30,17 @@ const char *argp_program_bug_address = "<jacobkoziej@gmail.com>";
 
 static const struct argp_option options[] = {
 	{
-		.name  = "color",
-		.key   = KEY_COLOR,
-		.arg   = "WHEN",
-		.doc   = "Override color output;\nWHEN is 'stdout', 'stderr', 'always', or 'never'"
+		.name = "color",
+		.key  = KEY_COLOR,
+		.arg  = "WHEN",
+		.doc  = "Override color output;\nWHEN is 'stdout', 'stderr', 'always', or 'never'"
+	},
+	{
+		.name  = "trace",
+		.key   = KEY_TRACE,
+		.flags = OPTION_ARG_OPTIONAL,
+		.arg   = "LEVEL",
+		.doc   = "Enable execution traces;\nLEVEL is '0..3'"
 	},
 	{0},
 };
@@ -89,6 +98,27 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 
 			break;
 
+		case KEY_TRACE:;
+			int level = JKCC_TRACE_LEVEL_LOW;
+
+			if (arg) {
+				level = atoi(arg);
+
+				if (level >= JKCC_TRACE_LEVEL_HIGH)
+					level = JKCC_TRACE_LEVEL_HIGH;
+
+				if (level < JKCC_TRACE_LEVEL_NONE)
+					level = JKCC_TRACE_LEVEL_NONE;
+			}
+
+			jkcc->config.trace = 1;
+
+			jkcc->trace.stream   = stderr;
+			jkcc->trace.ansi_sgr = jkcc->config.ansi_sgr_stderr;
+			jkcc->trace.level    = level;
+
+			break;
+
 		default:
 			return ARGP_ERR_UNKNOWN;
 	}
@@ -104,6 +134,23 @@ int main(int argc, char **argv)
 	// automatic config
 	jkcc.config.ansi_sgr_stdout = isatty(STDOUT_FILENO);
 	jkcc.config.ansi_sgr_stderr = isatty(STDERR_FILENO);
+
+	const char *JKCC_TRACE = getenv("JKCC_TRACE");
+	if (JKCC_TRACE) {
+		int level = atoi(JKCC_TRACE);
+
+		if (level > JKCC_TRACE_LEVEL_HIGH)
+			level = JKCC_TRACE_LEVEL_HIGH;
+
+		if (level < JKCC_TRACE_LEVEL_NONE)
+			level = JKCC_TRACE_LEVEL_NONE;
+
+		jkcc.config.trace = 1;
+
+		jkcc.trace.stream   = stderr;
+		jkcc.trace.ansi_sgr = jkcc.config.ansi_sgr_stderr;
+		jkcc.trace.level    = level;
+	}
 
 	argp_parse(&argp, argc, argv, 0, 0, &jkcc);
 
