1c44ad9daadfa575e0ffc8cf82911820016de076
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-14 14:44:25 -0400

    Add `list.h`

---
+++ include/jkcc/list.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * list.h -- linked list
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_LIST_H
+#define JKCC_LIST_H
+
+
+#endif  /* JCC_LIST_H */
