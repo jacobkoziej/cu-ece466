a9603cac06ac67de6cb800824f031108ba6a2b17
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-14 00:43:10 -0400

    Add `vector.h`

---
+++ include/jkcc/vector.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * vector.h -- dynamic size arrays
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_VECTOR_H
+#define JKCC_VECTOR_H
+
+
+#endif  /* JCC_VECTOR_H */
