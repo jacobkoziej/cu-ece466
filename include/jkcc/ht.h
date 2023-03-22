36a8813c258591de998c35daf33b775301f81c47
Jacob Koziej <jacobkoziej@gmail.com>
2023-03-22 15:04:55 -0400

    Add `ht.h`

---
+++ include/jkcc/ht.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * ht.h -- hash table
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_HT_H
+#define JKCC_HT_H
+
+
+#endif  /* JCC_HT_H */
