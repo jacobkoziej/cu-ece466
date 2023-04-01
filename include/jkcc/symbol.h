4d883075397e6c0f1f62e6c7f325135c2dc1fed7
Jacob Koziej <jacobkoziej@gmail.com>
2023-04-01 17:32:46 -0400

    Add `symbol.h`

---
+++ include/jkcc/symbol.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * symbol.h -- symbol table
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_SYMBOL_H
+#define JKCC_SYMBOL_H
+
+
+#endif  /* JCC_SYMBOL_H */
