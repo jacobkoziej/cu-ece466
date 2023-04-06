5325f86abe788adf1b9312eb90e953568af22823
Jacob Koziej <jacobkoziej@gmail.com>
2023-04-06 12:45:44 -0400

    Allocate symbol tables for `yyparse()`

--- src/parser.c
+++ src/parser.c
@@ -24,6 +24,7 @@ translation_unit_t *parse(parser_t *parser)
 {
 	translation_unit_t *translation_unit;
 	file_t             *file;
+	symbol_table_t     *identifier_symbol_table;
 	yyscan_t            yyscanner = NULL;
 	FILE               *stream    = NULL;
 
@@ -47,12 +48,26 @@ translation_unit_t *parse(parser_t *parser)
 	// avoid a double free in error recovery
 	file = NULL;
 
+	identifier_symbol_table = symbol_init();
+	if (!identifier_symbol_table) goto error;
+
+	if (vector_append(
+		&translation_unit->symbol_table.identifier,
+		&identifier_symbol_table)) goto error;
+
+	// avoid a double free in error recovery
+	identifier_symbol_table = NULL;
+
 	stream = (parser->path) ? fopen(parser->path, "r") : stdin;
 	if (!stream) goto error;
 
 	yyextra_t yyextra_data = {
 		.file           = *(file_t**) translation_unit->file.buf,
 		.file_allocated = &translation_unit->file,
+		.symbol_table   = {
+			.identifier = *(symbol_table_t**)
+				translation_unit->symbol_table.identifier.buf,
+		},
 	};
 
 	parser->yyextra_data = &yyextra_data;
@@ -74,33 +89,11 @@ error:
 
 	if (stream && (stream != stdin)) fclose(stream);
 
+	symbol_free(identifier_symbol_table);
+
 	if (file) {
 		if (file->path) free(file->path);
 		free(file);
-	}
-
-	if (translation_unit->symbol_table.identifier.buf) {
-		symbol_table_t **symbol =
-			translation_unit->symbol_table.identifier.buf;
-
-		for (
-			size_t i = 0;
-			i < translation_unit->symbol_table.identifier.use;
-			i++)
-			symbol_free(symbol[i]);
-
-		vector_free(&translation_unit->symbol_table.identifier);
-	}
-
-	if (translation_unit->file.buf) {
-		file_t **file = translation_unit->file.buf;
-
-		for (size_t i = 0; i < translation_unit->file.use; i++) {
-			free(file[i]->path);
-			free(file[i]);
-		}
-
-		vector_free(&translation_unit->file);
 	}
 
 	free(translation_unit);
