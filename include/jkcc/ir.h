cde009288a175ed608a122c29830686774501e18
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-05 19:52:24 -0400

    Add `ir.h`

---
+++ include/jkcc/ir.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * ir.h -- shitty intermediate representation
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_IR_H
+#define JKCC_IR_H
+
+
+#endif  /* JKCC_IR_H */
