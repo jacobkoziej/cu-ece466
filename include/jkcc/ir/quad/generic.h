5614feba504c6116d9ec665532402865b0eac58f
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-06 22:09:25 -0400

    Add `generic.h`

---
+++ include/jkcc/ir/quad/generic.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * generic.h -- generic quad
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_IR_QUAD_GENERIC_H
+#define JKCC_IR_QUAD_GENERIC_H
+
+
+#include <jkcc/ir/ir.h>
+
+
+#endif  /* JKCC_IR_QUAD_GENERIC_H */
