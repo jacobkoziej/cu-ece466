7a2189c8ae116665c881e9997ccffc477713d715
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-06 23:04:37 -0400

    Add `alloca.c`

---
+++ src/ir/quad/alloca.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * alloca.c -- stack allocation quad
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/ir/quad/alloca.h>
