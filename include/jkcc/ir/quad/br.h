108aaf91852315a7b23ff0c6d57d0313ca278b47
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-08 02:06:27 -0400

    Add `ir_quad_br_condition_t`

--- include/jkcc/ir/quad/br.h
+++ include/jkcc/ir/quad/br.h
@@ -11,4 +11,24 @@
 #include <jkcc/ir/ir.h>
 
 
+typedef enum ir_quad_br_condition_e {
+	IR_QUAD_BR_EQ,
+	IR_QUAD_BR_NE,
+	IR_QUAD_BR_HS,
+	IR_QUAD_BR_LO,
+	IR_QUAD_BR_MI,
+	IR_QUAD_BR_PL,
+	IR_QUAD_BR_VS,
+	IR_QUAD_BR_VC,
+	IR_QUAD_BR_HI,
+	IR_QUAD_BR_LS,
+	IR_QUAD_BR_GE,
+	IR_QUAD_BR_LT,
+	IR_QUAD_BR_GT,
+	IR_QUAD_BR_LE,
+	IR_QUAD_BR_AL,
+	IR_QUAD_BR_NV,
+} ir_quad_br_condition_t;
+
+
 #endif  /* JKCC_IR_QUAD_BR_H */
