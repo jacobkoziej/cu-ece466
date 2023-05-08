39a8e5ec5387182e2d10dc65afed7cd36710c386
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-08 01:46:06 -0400

    Add `cmp.h`

---
+++ include/jkcc/ir/quad/cmp.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * cmp.h -- comparison quad
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_IR_QUAD_CMP_H
+#define JKCC_IR_QUAD_CMP_H
+
+
+#include <jkcc/ir/ir.h>
+
+
+#endif  /* JKCC_IR_QUAD_CMP_H */
