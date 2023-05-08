e52e2997335261fd8801d3591ea5c47772d66ad1
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-07 20:58:56 -0400

    Define `ir_quad_alloca_t`

--- include/jkcc/ir/quad/alloca.h
+++ include/jkcc/ir/quad/alloca.h
@@ -10,11 +10,21 @@
 
 #include <jkcc/ir/ir.h>
 
+#include <stdint.h>
+
 
 typedef enum ir_quad_alloca_type_e {
 	IR_QUAD_ALLOCA_TYPE_I32,
 	IR_QUAD_ALLOCA_TYPE_PTR,
 } ir_quad_alloca_type_t;
+
+
+typedef struct ir_quad_alloc_s {
+	uintptr_t             dst;
+	ir_quad_alloca_type_t type;
+	size_t                align;
+	ir_quad_t             ir_quad;
+} ir_quad_alloca_t;
 
 
 #endif  /* JKCC_IR_QUAD_ALLOCA_H */
