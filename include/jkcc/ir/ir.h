b364916a95883592c94093e8b3ca9ec28cb8fb86
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-08 01:32:41 -0400

    Define `ir_quad_mov_t`

--- include/jkcc/ir/ir.h
+++ include/jkcc/ir/ir.h
@@ -29,6 +29,7 @@ typedef enum ir_quad_e {
 	IR_QUAD_ALLOCA,
 	IR_QUAD_BINOP,
 	IR_QUAD_LOAD,
+	IR_QUAD_MOV,
 	IR_QUAD_STORE,
 	IR_QUAD_TOTAL,
 } ir_quad_t;
--- include/jkcc/ir/quad/mov.h
+++ include/jkcc/ir/quad/mov.h
@@ -10,5 +10,17 @@
 
 #include <jkcc/ir/ir.h>
 
+#include <stddef.h>
+#include <stdint.h>
+
+
+typedef struct ir_quad_mov_s {
+	uintptr_t     dst;
+	ir_reg_type_t type;
+	uintptr_t     immediate;
+	size_t        align;
+	ir_quad_t     ir_quad;
+} ir_quad_mov_t;
+
 
 #endif  /* JKCC_IR_QUAD_MOV_H */
