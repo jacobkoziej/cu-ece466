362cc9a49f8c07831117124b38494145e99a09c5
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-09 02:08:47 -0400

    Add `declaration.c`

---
+++ src/ir/bb/declaration.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * declaration.c -- declaration basic block
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/ir/bb/declaration.h>
