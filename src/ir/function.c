b72c469538c9572525c38e99c935bba31535f9b9
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-08 23:19:52 -0400

    Allocate registers for arguments

--- src/ir/function.c
+++ src/ir/function.c
@@ -32,16 +32,72 @@ int ir_function_gen(
 	ir_function_t *ir_function,
 	ast_t         *ast_function)
 {
-	(void) ir_context;
-	(void) ir_function;
+	ir_function->declaration = ast_function;
 
 	vector_t *list;
 	{
 		ast_t *ast_list = ast_function_get_body(ast_function);
 
-		if (ast_list) return IR_ERROR_EMPTY_FUNCTION_BODY;
+		if (!ast_list) return IR_ERROR_EMPTY_FUNCTION_BODY;
 
 		list = ast_list_get_list(ast_list);
+	}
+
+	if (ht_init(&ir_context->reg.lookup, 0)) return IR_ERROR_NOMEM;
+	if (ht_init(&ir_context->reg.type, 0)) goto error_ht_init_reg_type;
+
+	// reset registers
+	ir_context->current.dst = 0;
+
+	vector_t *argv;
+	{
+		ast_t *ast_list;
+
+		ast_list = ast_function_get_parameter_list(ast_function);
+		if (ast_list) goto argv_done;
+
+		ast_list = ast_function_get_declaration_list(ast_function);
+
+argv_done:
+		argv = (ast_list) ? ast_list_get_list(ast_list) : NULL;
+	}
+
+	if (argv) {
+		ast_t **declaration = argv->buf;
+		for (size_t i = 0; i < argv->use; i++) {
+			ast_t *ast_type
+				= ast_declaration_get_type(declaration[i]);
+
+			ir_reg_type_t type;
+			switch (*ast_type) {
+				case AST_ARRAY:
+				case AST_POINTER:
+					type = IR_REG_TYPE_PTR;
+					break;
+
+				default:
+					// TODO: FIX THIS CRIME!
+					// everything's an i32... sigh
+					type = IR_REG_TYPE_I32;
+					break;
+			}
+
+			uintptr_t key = (uintptr_t) ast_type;
+			uintptr_t val = ir_context->current.dst++;
+
+			if (ht_insert(
+				&ir_context->reg.lookup,
+				&key,
+				sizeof(key),
+				(void*) val)) goto error_ht_insert_reg_lookup;
+
+			val = type;
+			if (ht_insert(
+				&ir_context->reg.type,
+				&key,
+				sizeof(key),
+				(void*) val)) goto error_ht_insert_reg_type;
+		}
 	}
 
 	ast_t **statement = list->buf;
@@ -49,4 +105,13 @@ int ir_function_gen(
 		(void) statement;
 
 	return 0;
+
+error_ht_insert_reg_type:
+error_ht_insert_reg_lookup:
+	ht_free(&ir_context->reg.type, NULL);
+
+error_ht_init_reg_type:
+	ht_free(&ir_context->reg.lookup, NULL);
+
+	return IR_ERROR_NOMEM;
 }
