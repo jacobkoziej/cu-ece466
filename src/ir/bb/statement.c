e1cc592c70782b8267906602f52fcdf28a948f29
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-09 00:50:55 -0400

    Add `statement.c`

---
+++ src/ir/bb/statement.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * statement.c -- statement basic block
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/ir/bb/statement.h>
