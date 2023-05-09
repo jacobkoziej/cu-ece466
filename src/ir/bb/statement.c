edc5cbb29b4fa9efa8a18016dae9291a33367b00
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-09 01:22:30 -0400

    Handle `nop` statements

--- src/ir/bb/statement.c
+++ src/ir/bb/statement.c
@@ -16,6 +16,20 @@ int ir_bb_statement_gen(ir_context_t *ir_context, ast_t *statement)
 
 	switch (*statement) {
 		default:
+			// handle statement
+			break;
+	}
+
+	switch (*statement) {
+		case AST_EMPTY:
+		case AST_IDENTIFIER:
+		case AST_TYPE:
+		case AST_TYPE_QUALIFIER:
+		case AST_TYPE_SPECIFIER:
+			// nop
+			return 0;
+
+		default:
 			return IR_ERROR_UNKNOWN_AST_NODE;
 	}
 
