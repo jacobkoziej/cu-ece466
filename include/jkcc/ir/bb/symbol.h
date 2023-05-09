0d7ae2e5c690251b20ebed7b40f5887a396a6b30
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-09 04:17:02 -0400

    Add `symbol.h`

---
+++ include/jkcc/ir/bb/symbol.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * symbol.h -- symbol basic block
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_IR_BB_SYMBOL_H
+#define JKCC_IR_BB_SYMBOL_H
+
+
+#include <jkcc/ir/ir.h>
+
+
+#endif  /* JKCC_IR_BB_SYMBOL_H */
