787a3e0cc88197f06a0fa27c114ef3b074b5870c
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-09 05:18:52 -0400

    Define `ir_bb_symbol_gen()`

--- src/ir/bb/symbol.c
+++ src/ir/bb/symbol.c
@@ -5,3 +5,34 @@
  */
 
 #include <jkcc/ir/bb/symbol.h>
+#include <jkcc/ir/ir.h>
+#include <jkcc/private/ir.h>
+
+#include <stdint.h>
+
+#include <jkcc/ast.h>
+#include <jkcc/ht.h>
+#include <jkcc/ir.h>
+
+
+int ir_bb_symbol_gen(
+	ir_context_t *ir_context,
+	ast_t        *ast)
+{
+	uintptr_t  key = (uintptr_t) ast_identifier_get_type(ast);
+	void      *val;
+
+	if (ht_get(
+		&ir_context->ir_function->reg.lookup,
+		&key,
+		sizeof(key),
+		&val)
+	) {
+		// TODO: handle loading global variables
+		return IR_ERROR_UNKNOWN_AST_NODE;
+	}
+
+	ir_context->result = (uintptr_t) val;
+
+	return 0;
+}
