0e8cc5f200395a670b28af81968e78ece8452bae
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-14 22:06:37 -0400

    Add `cmp.h`

---
+++ include/jkcc/ir/bb/cmp.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * cmp.h -- cmp basic block
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_IR_BB_CMP_H
+#define JKCC_IR_BB_CMP_H
+
+
+#include <jkcc/ir/ir.h>
+
+
+#endif  /* JKCC_IR_BB_CMP_H */
