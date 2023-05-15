f7d162d81c1d9dabf4601b9a9df7735d2c027813
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-15 02:18:17 -0400

    Add `sizeof.c`

---
+++ src/ir/bb/sizeof.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * sizeof.c -- sizeof basic block
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/ir/bb/sizeof.h>
