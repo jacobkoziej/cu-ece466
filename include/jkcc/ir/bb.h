e6452508d418704c01e859e41d62b09ec9c00c0e
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-14 22:45:37 -0400

    Add `if.h`

--- include/jkcc/ir/bb.h
+++ include/jkcc/ir/bb.h
@@ -14,6 +14,7 @@
 #include <jkcc/ir/bb/cmp.h>
 #include <jkcc/ir/bb/declaration.h>
 #include <jkcc/ir/bb/dereference.h>
+#include <jkcc/ir/bb/if.h>
 #include <jkcc/ir/bb/mov.h>
 #include <jkcc/ir/bb/statement.h>
 #include <jkcc/ir/bb/store.h>
---
+++ include/jkcc/ir/bb/if.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * if.h -- if basic block
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_IR_BB_IF_H
+#define JKCC_IR_BB_IF_H
+
+
+#include <jkcc/ir/ir.h>
+
+
+#endif  /* JKCC_IR_BB_IF_H */
