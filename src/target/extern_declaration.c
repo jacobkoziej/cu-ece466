7f52b5b2ede9915e05e0e239d22ff4e7784613c5
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-16 15:29:12 -0400

    Add `extern_declaration.c`

---
+++ src/target/extern_declaration.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * extern_declaration.c -- extern declaration target code generation
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/target/extern_declaration.h>
