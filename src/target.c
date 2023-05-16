cb13d61a5f12c9740708b6ab4fb287ae012732e6
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-16 12:41:42 -0400

    Add `target.c`

---
+++ src/target.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * target.c -- garbage target code generation
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/target.h>
