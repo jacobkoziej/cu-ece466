55c52d03d4b7426cc54df284be5c0251354a74a5
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-16 12:41:04 -0400

    Add `target.h`

---
+++ include/jkcc/target.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * target.h -- garbage target code generation
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef JKCC_TARGET_H
+#define JKCC_TARGET_H
+
+
+#endif  /* JKCC_TARGET_H */
