946a1b9999aedbc55539267665071a8bb1333e69
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-16 15:15:50 -0400

    Add `util.c`

---
+++ src/target/x86/util.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * util.c -- x86 target code generation utils
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/target/x86/util.h>
