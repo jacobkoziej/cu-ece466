83e88089606fa4ac6bc9d40e58981652d89c2521
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-16 11:35:34 -0400

    Add `string_literal.c`

---
+++ src/ir/bb/string_literal.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * string_literal.c -- string_literal basic block
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/ir/bb/string_literal.h>
