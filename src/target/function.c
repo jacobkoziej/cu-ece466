59838c459ce6cc69f80710e29542d0bae54ceec9
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-16 16:38:18 -0400

    Add `function.c`

---
+++ src/target/function.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * function.c -- function target code generation
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/target/function.h>
