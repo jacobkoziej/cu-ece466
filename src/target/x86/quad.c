babc9cc5c890c2b0f07e185c88cf61d264654d9f
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-16 20:17:56 -0400

    Add `quad.c`

---
+++ src/target/x86/quad.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * quad.c -- x86 target code generation for quads
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#include <jkcc/target/x86/quad.h>
